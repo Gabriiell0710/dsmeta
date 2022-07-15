5ae32bb2a7640e6cc586357c314cb349c8dd0a51
Gabriel Carvalho <gabriiell0710@gmail.com>
2022-07-15 13:13:03 -0300

    API test

---
+++ backend/src/main/java/com/devsuperior/demeta/repositories/SaleRepository.java
@@ -0,0 +1,9 @@
+package com.devsuperior.demeta.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.devsuperior.demeta.entities.Sale;
+
+public interface SaleRepository extends JpaRepository<Sale, Long> {
+
+}
